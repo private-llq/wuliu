ab1ba0dad2c1dff06c47fffd92b0f9f992d1e4bb
liulq <1649986432@qq.com>
2021-08-14 01:37:20 +0800

    修改xml-1

--- system/src/main/java/com/wuliu/system/config/StaticConfig.java
+++ system/src/main/java/com/wuliu/system/config/StaticConfig.java
@@ -1,31 +1,30 @@
-//package com.wuliu.system.config;
-//
-//import com.wuliu.common.util.DySmsHelper;
-//import org.jeecg.common.util.DySmsHelper;
-//import org.jeecg.modules.message.handle.impl.EmailSendMsgHandle;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-///**
-// * 设置静态参数初始化
-// */
-//@Configuration
-//public class StaticConfig {
-//    @Value("${jeecg.sms.accessKeyId}")
-//    private String accessKeyId;
-//    @Value("${jeecg.sms.accessKeySecret}")
-//    private String accessKeySecret;
-//    @Value(value = "${spring.mail.username}")
-//    private String emailFrom;
-//
-//    /**
-//     * 初始化
-//     */
-//    @Bean
-//    public void initStatic() {
-//        DySmsHelper.setAccessKeyId(accessKeyId);
-//        DySmsHelper.setAccessKeySecret(accessKeySecret);
-//        EmailSendMsgHandle.setEmailFrom(emailFrom);
-//    }
-//}
+package com.wuliu.system.config;
+
+import com.wuliu.common.util.DySmsHelper;
+import com.wuliu.system.modules.message.handle.impl.EmailSendMsgHandle;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 设置静态参数初始化
+ */
+@Configuration
+public class StaticConfig {
+    @Value("${jeecg.sms.accessKeyId}")
+    private String accessKeyId;
+    @Value("${jeecg.sms.accessKeySecret}")
+    private String accessKeySecret;
+    @Value(value = "${spring.mail.username}")
+    private String emailFrom;
+
+    /**
+     * 初始化
+     */
+    @Bean
+    public void initStatic() {
+        DySmsHelper.setAccessKeyId(accessKeyId);
+        DySmsHelper.setAccessKeySecret(accessKeySecret);
+        EmailSendMsgHandle.setEmailFrom(emailFrom);
+    }
+}
